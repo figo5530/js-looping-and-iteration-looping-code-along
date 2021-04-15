1e28a13208fec61e5d8ebb4dd73957d802e3c7a6
figo5530 <figooooo0221@gmail.com>
2021-04-15 16:09:40 +0800

    commit tree b03d16e57f36980359aae6d13a989a408fb46d4f

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, word) {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+        result.push(`Thank you, ${array[i]}, for the wonderful ${word} gift!`)
+    }
+    return result
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }
+}
